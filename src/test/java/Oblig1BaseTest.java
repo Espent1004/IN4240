647ea3df760ac73c0064634e104dc6c03d32d06f
espent1004 <espent1004@gmail.com>
2019-03-19 11:52:52 +0100

    Removing import

--- src/test/java/Oblig1BaseTest.java
+++ src/test/java/Oblig1BaseTest.java
@@ -12,7 +12,6 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 
 import java.io.IOException;
